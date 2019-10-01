b89a158b15fa22a1a902246c37386f2fdd0adca4
raiben23 <alexcaceres93@gmail.com>
2019-10-01 18:18:17 +0200

    Rehecho ejercicio Dibujo

--- Guions/[G] Clases y Objetos V - Tablas/Dibuix.java
+++ Guions/[G] Clases y Objetos V - Tablas/Dibuix.java
@@ -3,45 +3,92 @@
  */
 public class Dibuix {
 
-    private int Color;
-    private Rectangle2D rectangles[];
-    private int max_rec = 100;
+    private Rectangle2D taula_rectangle[];
+    private int num_max = 100, color, num_rec;
 
-    public Dibuix(Rectangle2D rectangles[]){
+    // Constructor
+    public Dibuix(Rectangle2D llista_rectangle[], int color) {
+        this.color = color;
 
-        this.rectangles = new Rectangle2D[max_rec];
+        taula_rectangle = new Rectangle2D[num_max];
 
-        if (rectangles.length <= max_rec){
-            for (int i = 0; i < rectangles.length; i++) {
-                this.rectangles[i] = new Rectangle2D(rectangle[i]);
+        if (llista_rectangle.length <= num_max) {
+            for (int i = 0; i < llista_rectangle.length; i++) {
+                num_rec = i; // Contador de rectangulos en array
+                taula_rectangle[i] = new Rectangle2D(llista_rectangle[i]);
             }
         }
-        else for (int i = 0; i < max_rec; i++) {
-            this.rectangles[i] = new Rectangle2D(rectangles[i]);
+
+        else {
+            for (int i = 0; i < num_max; i++) {
+                num_rec = i; // Contador de rectangulos en array
+                taula_rectangle[i] = new Rectangle2D(llista_rectangle[i]);
+            }
         }
     }
 
-    public void setColor(int Color){
-        this.Color = Color;
+    // GETTERS Y SETTERS
+
+    public void setRectangles() {
+
     }
 
-    public void setMaxRectangles(int max_rec){
-        this.max_rec = max_rec;
+    public void setColor() {
+
     }
 
-    public int getColor(){
-        return Color;
+    public void setMaxRec() {
+
     }
 
-    public int getMaxRectangles(){
-        return max_rec;
+    public void setNumRec(){
+
+    }
+
+    public Rectangle2D getRectangles() {
+
+    }
+
+    public int getColor() {
+
+    }
+
+    public int getMaxRec() {
+
+    }
+
+    public int getNumRec() {
+
+    }
+
+    // METODOS DE CLASE
+
+    public void addRec(Rectangle2D rectangle){                      // Sumamos contador y añadimos objeto
+        if(num_rec<num_max){
+            num_rec++;
+            taula_rectangle[num_rec] = new Rectangle2D(rectangle);
+        }
+
+        else System.out.println("La tabla esta llena!");
+    }
+
+    public void deleteRec(int c){
+        
+        if (c < num_rec){
+            for (int i = c; i <= num_rec; i++) {
+                taula_rectangle[i] = taula_rectangle[i++];
+            }
+            //taula_rectangle[num_rec] = taula_rectangle[num_rec++];// hago que valga lo del siguiente (nulo)
+            num_rec--;//luego resto uno del numero de rectangulo
+        }
+        
+        else (c == num_rec){
+
+        }
+    }
+
+    public void subRec(int c, Rectangle2D rectangle){
+        taula_rectangle[c] = new Rectangle2D(rectangle);
     }
     
-    public Rectangle2D getRectangle(int i){
-        return rectangles[i];
-    }
-
-    public int num_Rectangles(){
-        return rectangles.length;
-    }
 }
