8c1905a14ee111caf5bc91b3ff6d99ad7a4873f1
raiben23 <alexcaceres93@gmail.com>
2019-10-03 00:10:40 +0200

    Implementado Arraylist

--- Guions/[G] Clases y Objetos V - Tablas/Dibuix.java
+++ Guions/[G] Clases y Objetos V - Tablas/Dibuix.java
@@ -1,28 +1,32 @@
+import java.util.ArrayList;
+
 /**
  * Dibuix
  */
 public class Dibuix {
 
-    private Rectangle2D taula_rectangle[];
+    //private Rectangle2D taula_rectangle[];
+
+    ArrayList<Rectangle2D> taula_rectangle = new ArrayList<Rectangle2D>();
     private int num_max = 100, color, num_rec;
 
     // Constructor
-    public Dibuix(Rectangle2D llista_rectangle[], int color) {
+    public Dibuix(ArrayList<Rectangle2D> llista_rectangle, int color) {
         this.color = color;
 
-        taula_rectangle = new Rectangle2D[num_max];
+       // taula_rectangle = new Rectangle2D[num_max];
 
-        if (llista_rectangle.length <= num_max) {
-            for (int i = 0; i < llista_rectangle.length; i++) {
-                num_rec = i; // Contador de rectangulos en array
-                taula_rectangle[i] = new Rectangle2D(llista_rectangle[i]);
+        if (llista_rectangle.size() <= num_max) {
+            for (int i = 0; i < llista_rectangle.size(); i++) {
+                //num_rec = i; // Contador de rectangulos en array
+                taula_rectangle.add(llista_rectangle.get(i));
             }
         }
 
         else {
             for (int i = 0; i < num_max; i++) {
-                num_rec = i; // Contador de rectangulos en array
-                taula_rectangle[i] = new Rectangle2D(llista_rectangle[i]);
+                //num_rec = i; // Contador de rectangulos en array
+                taula_rectangle.add(llista_rectangle.get(i));
             }
         }
     }
@@ -64,31 +68,38 @@ public class Dibuix {
     // METODOS DE CLASE
 
     public void addRec(Rectangle2D rectangle){                      // Sumamos contador y añadimos objeto
-        if(num_rec<num_max){
-            num_rec++;
-            taula_rectangle[num_rec] = new Rectangle2D(rectangle);
+        if(taula_rectangle.size()<num_max){
+           // num_rec++;
+            taula_rectangle.add(rectangle);
         }
 
         else System.out.println("La tabla esta llena!");
     }
 
     public void deleteRec(int c){
-        
-        if (c < num_rec){
-            for (int i = c; i <= num_rec; i++) {
-                taula_rectangle[i] = taula_rectangle[i++];
-            }
-            //taula_rectangle[num_rec] = taula_rectangle[num_rec++];// hago que valga lo del siguiente (nulo)
-            num_rec--;//luego resto uno del numero de rectangulo
-        }
-        
-        else (c == num_rec){
 
-        }
+        taula_rectangle.remove(c);
+    
+        
+    //     if (c < num_rec){
+    //         for (int i = c; i <= num_rec; i++) {
+    //             taula_rectangle[i] = taula_rectangle[i++];
+    //         }
+    //         //taula_rectangle[num_rec] = taula_rectangle[num_rec++];// hago que valga lo del siguiente (nulo)
+    //         num_rec--;//luego resto uno del numero de rectangulo
+    //     }
+        
+    //     else (c == num_rec){
+
+    //     }
+    // }
+
     }
 
     public void subRec(int c, Rectangle2D rectangle){
-        taula_rectangle[c] = new Rectangle2D(rectangle);
+       // taula_rectangle[c] = new Rectangle2D(rectangle);
+
+       taula_rectangle.set(c, rectangle);
     }
     
 }
