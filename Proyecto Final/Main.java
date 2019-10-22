5600bf5f544991df7aa589c3202503266c3a73c3
raiben23 <alexcaceres93@gmail.com>
2019-10-22 11:59:02 +0200

    Continuando Proyecto final...

--- Proyecto Final/Main.java
+++ Proyecto Final/Main.java
@@ -1,43 +1,61 @@
-import java.util.*;
-
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
 /**
  * main
  */
 public class Main {
 
-    public static void print(Object a){
+    static void print(Object a){
         System.out.println(a);
     }
+    static void conexionFichero(){
+    }
+    static Carrito ejemploCarrito(){
+        
+       Fabricante F = new Fabricante(0, "Samgung", 935628130);
+       Cliente C = new Cliente(0,"Alex","Caceres","4785 4455 6684 4566");
+       ArrayList<Componente> Productos = new ArrayList<Componente>();
 
+       Productos.add(new Componente(F,0,"Monitor 29'",250));
+       Productos.add(new Componente(F,1,"Teclado Motospeed",80));
+       Productos.add(new Componente(F,2,"NVIDIA GeForce GTX 2060",750));
+       Productos.add(new Componente(F,3,"Memoria RAM Corsair 8GB",40));
+       Productos.add(new Componente(F,4,"Raton inalambrico Razer",50));
+       Productos.add(new Componente(F,5,"Cooler Razer CPU",60));
+       Productos.add(new Componente(F,6,"Intel i7 9124 3.2GHz",350));
+       Productos.add(new Componente(F,7,"Placa Base Asus G2345",120));
+       Productos.add(new Componente(F,8,"Fuente de alimentación Corsair 750W",70));
+       Productos.add(new Componente(F,9,"Tarjeta Wi-Fi StA",20)); 
+
+       Carrito Carro = new Carrito(Productos, new Date(), C);
+       return Carro;
+    }
+    static void etapa1(){
+        print("\n\n\n\n");
+        Carrito Carro = new Carrito();
+        Carro = ejemploCarrito();
+        print("Carrito:    " + new Date());
+        print(Carro);
+        print("Carrito: Eliminados productos 2,3,4,5    " + new Date());
+        print(Carro.eliminarProducto(2));
+        print(Carro.eliminarProducto(3));
+        print(Carro.eliminarProducto(4));
+        print(Carro.eliminarProducto(5));
+        print(Carro);
+        print("Carrito: Intentando eliminar producto 5 (que no se encuentra en el carrito)");
+        print(Carro.eliminarProducto(5));
+        print(Carro);
+        print("Realizada Venta");
+        Venta venta = new Venta();
+        venta = Carro.comprar(new Date(),"Tarjeta de credito");
+        print(venta);
+    }
+    
+    
     public static void main(String[] args) {
 
-        print("");
-        print("");
-        print("");
-        print("");
-        Cliente a = new Cliente(1,"Alex", "Caceres", "D1243");
-        Fabricante b = new Fabricante(1,"Samsung",912345621);
-        Componente c = new Componente(b,1,"Monitor 29'",300.0);
-        ArrayList<Componente> productos = new ArrayList<Componente>();
-        productos.add(new Componente(b,2,"producto1",329));
-        productos.add(new Componente(b,3,"producto2",310));
-        productos.add(new Componente(b,4,"producto3",320));
-        productos.add(new Componente(b,5,"producto4",330));
-        productos.add(new Componente(b,6,"producto5",340));
-        productos.add(new Componente(b,7,"producto6",350));
-
-        //productos.forEach(p->print(p));
-
-        Carrito carrito = new Carrito(productos,new Date(),a);
-
-        //print(productos.size());
-        
-        // print("PRUEBAS: CLIENTE, FABRICANTE, PRODUCTO");
-        // print(a);
-        // print(b);
-        // print(c);
-        print("PRUEBAS: CARRITO DE LA COMPRA");
-        print(carrito);
-
+            etapa1();
+    
     }
 }
